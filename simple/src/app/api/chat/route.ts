4b55f1cbc3941c11ad304d7f4f0bc307ba9cb14f
Rabi Shanker Guha <guha.rabishankar@gmail.com>
2025-02-13 17:17:43 -0800

    Add the simplest possible example of using crayon

---
+++ simple/src/app/api/chat/route.ts
@@ -0,0 +1,57 @@
+import { NextRequest, NextResponse } from "next/server";
+import OpenAI from "openai";
+import { fromOpenAICompletion } from "@crayonai/stream";
+import { CreateMessage } from "@crayonai/react-core";
+import { ChatCompletionMessageParam } from "openai/resources/chat/completions";
+
+export const TemplatesJsonSchema = {
+  type: "object",
+  properties: {
+    response: {
+      type: "array",
+      items: {
+        oneOf: [
+          {
+            type: "string",
+            description: "text message to be displayed to the user",
+          },
+        ],
+      },
+    },
+  },
+} as const;
+
+function mapCreateMessageToOpenAIMessage(
+  message: CreateMessage
+): ChatCompletionMessageParam {
+  return {
+    role: "user",
+    content: message.message,
+  };
+}
+
+export async function POST(req: NextRequest) {
+  const { message } = await req.json();
+  const client = new OpenAI();
+  const llmStream = await client.chat.completions.create({
+    model: "gpt-4o",
+    messages: [mapCreateMessageToOpenAIMessage(message)],
+    stream: true,
+    response_format: {
+      type: "json_schema",
+      json_schema: {
+        name: "json_schema",
+        schema: TemplatesJsonSchema,
+      },
+    },
+  });
+  const responseStream = fromOpenAICompletion(llmStream);
+  // @ts-expect-error fix this
+  return new NextResponse(responseStream, {
+    headers: {
+      "Content-Type": "text/event-stream",
+      "Cache-Control": "no-cache, no-transform",
+      Connection: "keep-alive",
+    },
+  });
+}
