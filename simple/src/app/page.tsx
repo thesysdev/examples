d91165ca359284c37624583ad3a0e1d0ed7c9a12
Rabi Shanker Guha <guha.rabishankar@gmail.com>
2025-02-13 17:17:43 -0800

    Add the simplest possible example of using crayon

---
+++ simple/src/app/page.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import type { CreateMessage } from "@crayonai/react-core";
+import { CrayonChat } from "@crayonai/react-ui";
+import "@crayonai/react-ui/styles/index.css";
+
+const processMessage = async ({
+  threadId,
+  message,
+  abortController,
+}: {
+  threadId: string;
+  message: CreateMessage;
+  abortController: AbortController;
+}) => {
+  const response = await fetch("/api/chat", {
+    method: "POST",
+    body: JSON.stringify({ threadId, message }),
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "text/event-stream",
+    },
+    signal: abortController.signal,
+  });
+  return response;
+};
+
+export default function Home() {
+  return (
+    <CrayonChat
+      agentName="Crayon"
+      logoUrl="https://crayonai.org/img/logo.png"
+      processMessage={processMessage}
+    />
+  );
+}
