85baf11d670408b2a39a2f87a2feac8a17f5e8e2
Hrijul Bhatnagar <hrijul@thesys.dev>
2025-04-08 08:10:07 +0530

    Create an example app using C1 and tool calling for image search (#4)

---
+++ c1-chat/README.md
@@ -0,0 +1,29 @@
+# C1 App Template
+This is a [C1 by Thesys](https://thesys.dev) project bootstrapped with `create-next-app`
+
+## Getting Started
+
+First, generate a new API key from [Thesys Console](https://chat.thesys.dev/console/keys) and then set it your environment variable.
+
+```bash
+export THESYS_API_KEY=<your-api-key>
+```
+
+Then, run the development server:
+
+```bash
+pnpm dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+
+You can start editing your responses by modifying the system prompt in `src/app/api/chat/route.ts`.
+
+
+## Learn More
+
+To learn more about Thesys C1, take a look at the [C1 Documentation](https://docs.thesys.dev) - learn about Thesys C1.
+
+## Deploy on Vercel
+
+The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme).
---
+++ c1-chat/src/app/api/chat/tools.ts
@@ -0,0 +1,37 @@
+import type { RunnableToolFunctionWithParse } from "openai/lib/RunnableFunction.mjs";
+import type { RunnableToolFunctionWithoutParse } from "openai/lib/RunnableFunction.mjs";
+import { z } from "zod";
+import { zodToJsonSchema } from "zod-to-json-schema";
+import GoogleImages from "google-images";
+import type { JSONSchema } from "openai/lib/jsonschema.mjs";
+
+const client = new GoogleImages(
+  process.env.GOOGLE_CSE_ID!,
+  process.env.GOOGLE_API_KEY!
+);
+
+export const tools: (
+  | RunnableToolFunctionWithoutParse
+  | RunnableToolFunctionWithParse<{ altText: string }>
+)[] = [
+  {
+    type: "function",
+    function: {
+      name: "getImageSrc",
+      description: "Get the image src for the given alt text",
+      parse: JSON.parse,
+      parameters: zodToJsonSchema(
+        z.object({
+          altText: z.string().describe("The alt text of the image"),
+        })
+      ) as JSONSchema,
+      function: async ({ altText }: { altText: string }) => {
+        const results = await client.search(altText, {
+          size: "medium",
+        });
+        return results[0].url;
+      },
+      strict: true,
+    },
+  },
+];
