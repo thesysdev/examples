89410f743b39a1b6ccd3d888f9a6ef4621fe96e0
Rabi Shanker Guha <guha.rabishankar@gmail.com>
2025-04-10 19:45:33 +0530

    Override styles in forms to show how to customize C1 chat

--- forms/src/app/page.tsx
+++ forms/src/app/page.tsx
@@ -2,6 +2,8 @@
 
 import "@crayonai/react-ui/styles/index.css";
 import { C1Chat } from "@thesysai/genui-sdk";
+// Import the override styles. This should be the last import.
+import "./c1.css";
 
 export default function Home() {
   return <C1Chat theme={{ mode: "dark" }} apiUrl="/api/chat" />;
