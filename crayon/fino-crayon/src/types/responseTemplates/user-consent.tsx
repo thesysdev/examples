a42401c688767e7697782d83a717147d876b8f0f
Rabi Shanker Guha <guha.rabishankar@gmail.com>
2025-11-13 19:31:06 +0530

    Remove crayon folder from examples

--- crayon/fino-crayon/src/types/responseTemplates/user-consent.tsx
+++
@@ -1,6 +0,0 @@
-import { z } from "zod";
-export const UserConsentSchema = z.object({
-  explanation: z.string(),
-});
-
-export type UserConsentProps = z.infer<typeof UserConsentSchema>;
--- crayon/template-fastapi/ui/src/templates/text.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Markdown from "react-markdown";
-
-// Template for text response
-export const TextTemplate: React.FC<{ children: React.ReactNode }> = ({
-  children,
-}) => {
-  return <Markdown>{children as string}</Markdown>;
-};
